94dba5f7674e04a18217e0dc2334110e5db01ccd
Frostblazer <42157097+Frostblazer@users.noreply.github.com>
2018-12-07 19:28:46 -0500

    added files

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    //Hello World Program
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string userName = "";
+            int userAge = 0;
+            int currentYear = 0;
+
+            Console.Write("Please enter your name: ");
+            userName = Console.ReadLine();
+            Console.WriteLine("Hello " + userName + "!");
+            Console.Write(userName + ", Please enter your age: ");
+            userAge = Convert.ToInt32(Console.ReadLine());
+            Console.Write(userName + " Please enter the current year: ");
+            currentYear = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your name is " + userName+ ". The current year is " + currentYear);
+        }
+    }
+}
